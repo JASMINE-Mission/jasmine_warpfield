bugfix: r2 normalization
The distance from the distortion center, r2, was wrongly normalized,
resulting in different distortion scales in four plates.

The normalization parameter r0 is defined in generating a distortion function.
Thus, the distortion scale is fixed among the plates.

challenge/case8/generate_case8.py
@@ -30,12 +30,14 @@ class PositionAngle(Longitude):
 def distortion_generator(c, K):
   from functools import reduce
   from operator import add
+  cx,cy = c
+  r0 = max((cx-20000)**2+(cy-20000)**2, (cx+20000)**2+(cy-20000)**2,
+           (cx-20000)**2+(cy+20000)**2, (cx+20000)**2+(cy+20000)**2)
   def distortion(position):
     position = np.array(position)
     center = np.array(c).reshape((2,1))
     ka = K.reshape((-1,2))
-    r2 = np.square(position-center).sum(axis=0,keepdims=True)
-    r2 /= r2.max()
+    r2 = np.square(position-center).sum(axis=0,keepdims=True)/r0
     Kp = 1-reduce(add,[k.reshape((2,1))*(r2**(n+1)) for n,k in enumerate(ka)])
     return (position-center)/Kp+center
   return distortion