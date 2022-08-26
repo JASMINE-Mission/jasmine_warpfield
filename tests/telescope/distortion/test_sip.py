a875d9bf6b50aa8ea06788d4b1bda1c819dcb3ca
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2022-08-26 23:33:27 +0900

    code format revised

--- tests/telescope/distortion/test_sip.py
+++ tests/telescope/distortion/test_sip.py
@@ -21,6 +21,7 @@ def test_sip_distortion_zero(seed):
     converted = sip(position)
     assert converted == approx(position)
 
+
 @given(integers(0, 2**31))
 def test_altsip_distortion_zero(seed):
     Nsrc = 1000
@@ -28,7 +29,7 @@ def test_altsip_distortion_zero(seed):
     A = np.zeros((order + 1, order + 1))
     B = np.zeros((order + 1, order + 1))
     np.random.seed(seed)
-    c = np.random.uniform(-10,10, size=(2, 1))
+    c = np.random.uniform(-10, 10, size=(2, 1))
     position = np.random.uniform(-1000, 1000, size=(2, Nsrc))
 
     sip = AltSipDistortion(order, c, A, B)
