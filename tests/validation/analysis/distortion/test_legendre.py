67dc6682ae9b4bc84ea495865d6ce238efd43af2
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2022-09-15 14:36:22 +0900

    add new tets cases

--- tests/validation/analysis/distortion/test_legendre.py
+++ tests/validation/analysis/distortion/test_legendre.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from pytest import approx
 from hypothesis import given, settings
-from hypothesis.strategies import integers, lists, floats
+from hypothesis.strategies import integers, lists, floats, composite
 import jax.numpy as jnp
 import numpy as np
 import numpy.polynomial.legendre as legendre
@@ -14,17 +14,25 @@ def seeds():
     return integers(0, 2**32 - 1)
 
 
-def arrays():
-    return lists(floats(-1, 1), min_size=1, max_size=1000)
+@composite
+def arrays(draw):
+    return jnp.array(draw(lists(floats(-1, 1), min_size=1, max_size=10)))
 
 
-def random(seed):
-    return np.random.default_rng(seed=seed)
+@composite
+def rngs(draw):
+    return np.random.default_rng(seed=draw(seeds()))
 
 
 @settings(deadline=None)
-@given(arrays(), seeds())
-def test_legval(arr, seed):
-    x = jnp.array(arr)
-    coeff = random(seed).normal(size=(16))
+@given(arrays(), rngs())
+def test_legval(x, rng):
+    coeff = rng.normal(size=(16))
     assert legval(x, coeff) == approx(legendre.legval(x, coeff))
+
+
+@settings(deadline=None)
+@given(arrays(), rngs())
+def test_legval2d(x, rng):
+    coeff = rng.normal(size=(16))
+    assert legval2d(x, x, coeff) == approx(legendre.legval2d(x, x, coeff))
