15c633e9a457975280b8b75bcbec57ef03ce5949
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2022-12-08 15:22:21 +0900

    update a test case generate script

--- challenge/baseline/generate.py
+++ challenge/baseline/generate.py
@@ -38,9 +38,14 @@ def generate(fields, catalog, plate, filename):
     n_coeff = 5
     coeff_x = np.zeros((n_coeff + 1, n_coeff + 1))
     coeff_y = np.zeros((n_coeff + 1, n_coeff + 1))
-    coeff_x[1, 0] = coeff_y[0, 1] = 3.0
-    coeff_x[3, 0] = coeff_y[0, 3] = 1.0
-    coeff_x[5, 0] = coeff_y[0, 5] = 0.3
+    coeff_x[1, 0] = +2.2
+    coeff_y[0, 1] = +2.7
+    coeff_x[1, 1] = +0.2
+    coeff_y[1, 1] = +0.3
+    coeff_x[3, 0] = +1.0
+    coeff_y[0, 3] = -1.0
+    coeff_x[5, 0] = +0.3
+    coeff_y[0, 5] = +0.5
 
     n_field = []
     n_block = []
@@ -49,6 +54,7 @@ def generate(fields, catalog, plate, filename):
     tel_ra  = []
     tel_dec = []
     tel_pa  = []
+    tel_foc = []
 
     ## replace with a large single detector
     detector = Detector(40960, 40960, 1 * u.um)
@@ -72,14 +78,14 @@ def generate(fields, catalog, plate, filename):
             dpa = center.icrs.position_angle(pos)
 
             jasmine = get_jasmine(center, pa)
+            r_efl = 1 + 1e-3 * np.random.normal()
+            jasmine.optics.focal_length *= r_efl
             jasmine.detectors = [detector,]
 
             vanilla = jasmine.optics.imaging(sources)
             vanilla['catalog_id'] = vanilla.index.to_series()
             vanilla = vanilla.loc[:, ['x', 'y', 'catalog_id']]
 
-            # r_efl = 1 + 1e-4 * np.random.normal()
-            # jasmine.optics.focal_length *= r_efl
             distortion = LegendreDistortion(n_coeff, coeff_x, coeff_y)
             jasmine.set_distortion(distortion)
             distorted = jasmine.observe(sources)[0]
@@ -109,6 +115,7 @@ def generate(fields, catalog, plate, filename):
             tel_ra.append(center.icrs.ra.deg)
             tel_dec.append(center.icrs.dec.deg)
             tel_pa.append(PositionAngle(pa + dpa).deg)
+            tel_foc.append(jasmine.optics.focal_length.to_value('m'))
 
     ## compile catalog
     catalog = pd.concat(catalog)
@@ -157,8 +164,9 @@ def generate(fields, catalog, plate, filename):
         np.array(tel_ra) * u.deg,
         np.array(tel_dec) * u.deg,
         np.array(tel_pa) * u.deg,
+        np.array(tel_foc) * u.m,
     ],
-        names=('field', 'block', 'plate', 'ra', 'dec', 'pa'),
+        names=('field', 'block', 'plate', 'ra', 'dec', 'pa', 'foc'),
         meta={'comments': ['The attitudes of the telescope.']},
     )
     print(table)
@@ -171,10 +179,15 @@ def generate(fields, catalog, plate, filename):
 
 if __name__ == '__main__':
     parser = ap(description='Generate baseline challenges')
-    parser.add_argument('--catalog',
-                        type=str,
-                        default='source_list.txt',
-                        help='filename of the source list')
+
+    parser.add_argument(
+        'output', type=str, help='output filename')
+    parser.add_argument(
+        '-n', '--n_plate', type=int, default=4,
+        help='the number of plates per field')
+    parser.add_argument(
+        '--catalog', type=str, default='source_list.txt',
+        help='filename of the source list')
 
     args = parser.parse_args()
 
@@ -182,7 +195,7 @@ if __name__ == '__main__':
     stride = Angle(15 * u.arcmin)
     fields = get_fields(stride)
 
-    plate = 1
+    plate = args.n_plate
     catalog = args.catalog
-    filename = f'baseline.txt'
+    filename = args.output
     generate(fields, catalog, plate, filename)
