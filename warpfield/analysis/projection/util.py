dccda92fb1e49689cd7331cd7f3a367653363fa6
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2023-05-11 17:46:12 +0900

    do not separate scales

--- warpfield/analysis/projection/util.py
+++ warpfield/analysis/projection/util.py
@@ -31,7 +31,7 @@ def generate_conversion(xfunc, yfunc):
 
 
 def generate_projection(func):
-    def inner_func(tel_ra, tel_dec, tel_pa, ra, dec, sx, sy):
+    def inner_func(tel_ra, tel_dec, tel_pa, ra, dec, scale):
         ''' Gnomonic projection of the spherical coordinates
 
         Arguments:
@@ -40,8 +40,7 @@ def generate_projection(func):
           tel_pa: A position angle of the telescope in degree.
           ra: A right ascension of the target in degree.
           dec: A declination of the target in degree.
-          sx: A physical scale of the focal plane along x (mm/deg).
-          sx: A physical scale of the focal plane along y (mm/deg).
+          scale: A physical scale of the focal plane (mm/deg).
 
         Returns:
           Converted coordinates on the focal plane
@@ -52,5 +51,5 @@ def generate_projection(func):
         ra = degree_to_radian(ra)
         dec = degree_to_radian(dec)
         X, Y = func(tel_ra, tel_dec, ra, dec)
-        return (rotation_matrix(-tel_pa) @ jnp.stack([sx * X, sy * Y])).T
+        return (rotation_matrix(-tel_pa) @ jnp.stack([X, Y])).T * scale
     return inner_func
