d5f15c30dc5f5c177655470cfc6146f41730b08a
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2026-07-17 16:44:14 +0900

    feat: add plate carree projection

--- warpfield/projection/__init__.py
+++ warpfield/projection/__init__.py
@@ -3,12 +3,14 @@
 ''' Projection of the spherical coordinate onto the focal plane '''
 
 from .base import Projection
+from .cylindrical import CylindricalProjection
 from .equidistant import EquidistantProjection
 from .gnomonic import GnomonicProjection
 from .orthographic import OrthographicProjection
 
 
 __all__ = [
+    'CylindricalProjection',
     'EquidistantProjection',
     'GnomonicProjection',
     'OrthographicProjection',
